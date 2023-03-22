33dbe4882c093fc8ec2e5a87bc0e2c2b626fe0e2
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-03-22 15:48:00 +0100

    can display degrees

---
+++ Compass/Compass/Services/GpsService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Compass.Services;
+
+public class GpsService
+{
+    private const double EarthRadiusMeters = 6371000;
+
+    public GpsService()
+	{
+	}
+
+	/// <summary>
+	/// Return the distance between 2 gps location
+	/// </summary>
+	/// <param name="location1"></param>
+	/// <param name="location2"></param>
+	/// <returns>The distance in meters</returns>
+	public int GetDistance(Location location1, Location location2)
+    {
+        double lat1_rad = location1.Latitude * Math.PI / 180;
+        double lon1_rad = location1.Longitude * Math.PI / 180;
+        double lat2_rad = location2.Latitude * Math.PI / 180;
+        double lon2_rad = location2.Longitude * Math.PI / 180;
+
+        double dlat = lat2_rad - lat1_rad;
+        double dlon = lon2_rad - lon1_rad;
+
+        double a = Math.Pow(Math.Sin(dlat / 2), 2) +
+                   Math.Cos(lat1_rad) * Math.Cos(lat2_rad) *
+                   Math.Pow(Math.Sin(dlon / 2), 2);
+
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        double distanceMeters = EarthRadiusMeters * c;
+
+        return (int)Math.Round(distanceMeters);
+    }
+}
+
---
+++ Compass/Compass/ViewModels/CompassViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Maui.Devices.Sensors;
+
+namespace Compass.ViewModels;
+
+public class CompassViewModel : BaseViewModel
+{
+	private string _currentLocation;
+	public string CurrentLocation
+	{
+		get => _currentLocation;
+		set
+		{
+            _currentLocation = value;
+            OnPropertyChanged(nameof(CurrentLocation));
+		} 
+	}
+
+    private double _compassValue;
+    public double CompassValue
+    {
+        get => _compassValue;
+        set
+        {
+            _compassValue = value;
+            OnPropertyChanged(nameof(CompassValue));
+        }
+    }
+
+    public Command OnGetCurrentLocationCommand { get; private set; }
+
+
+	public CompassViewModel()
+	{
+		OnGetCurrentLocationCommand = new Command(x => GetCurrentLocation());
+
+		if (Microsoft.Maui.Devices.Sensors.Compass.Default.IsSupported)
+		{
+            if (!Microsoft.Maui.Devices.Sensors.Compass.Default.IsMonitoring)
+            {
+                // Turn on compass
+                Microsoft.Maui.Devices.Sensors.Compass.Default.ReadingChanged += Compass_ReadingChanged;
+                Microsoft.Maui.Devices.Sensors.Compass.Default.Start(SensorSpeed.UI);
+            }
+            else
+            {
+                // Turn off compass
+                Microsoft.Maui.Devices.Sensors.Compass.Default.Stop();
+                Microsoft.Maui.Devices.Sensors.Compass.Default.ReadingChanged -= Compass_ReadingChanged;
+            }
+        }
+
+    }
+
+	public async void GetCurrentLocation()
+	{
+		try
+		{
+            Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+			CurrentLocation = $"{location.Latitude} {location.Longitude}";
+        }
+		catch(Exception e)
+		{
+
+		}
+	}
+
+    private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+    {
+        CompassValue = e.Reading.HeadingMagneticNorth;
+    }
+}
+
