eb1484092d99d73e41382df1c83a266c08ee41f4
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-03-28 19:41:24 +0200

    can click on pin and get the id

--- Compass/Compass/ViewModels/MapViewModel.cs
+++ Compass/Compass/ViewModels/MapViewModel.cs
@@ -1,15 +1,40 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Compass.Models.Entities;
+using Compass.Models.Wrappers;
+using Compass.Repositories.Interfaces;
 using Compass.Services.Interfaces;
 using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 
 namespace Compass.ViewModels;
 
 public class MapViewModel : BaseViewModel
 {
-    public MapViewModel(INavigationService navigationService) : base(navigationService)
+    private readonly IRepository<LocationEntity> _locationRepository;
+    private readonly IGpsService _gpsService;
+
+    public MapViewModel(
+        INavigationService navigationService,
+        IRepository<LocationEntity> locationRepository,
+        IGpsService gpsService) : base(navigationService)
     {
         CloseMapCommand = new Command(async x => await OnCloseMapCommand());
         MapClickedCommand = new Command(async x => await OnMapClickedCommand());
+
+        _locationRepository = locationRepository;
+        _gpsService = gpsService;
+    }
+
+    public async override Task OnNavigatedTo()
+    {
+        await base.OnNavigatedTo();
+
+        var locationsWrapper = _locationRepository.Get().Select(x => new LocationWrapper(x));
+
+        Locations = new ObservableCollection<LocationWrapper>(locationsWrapper);
+
+        //Location = await _gpsService.GetLocationAsync();
     }
 
     public Command CloseMapCommand { get; private set; }
@@ -21,7 +46,39 @@ public class MapViewModel : BaseViewModel
     public Command MapClickedCommand { get; private set; }
     private async Task OnMapClickedCommand()
     {
-        await NavigationService.CloseModalAsync();
+        //await NavigationService.CloseModalAsync();
     }
+
+    public async Task OnPinClickedAsync(long id)
+    {
+        //await NavigationService.CloseModalAsync();
+    }
+
+
+    #region Locations
+    private ObservableCollection<LocationWrapper> _locations;
+    public ObservableCollection<LocationWrapper> Locations
+    {
+        get => _locations;
+        set
+        {
+            _locations = value;
+            OnPropertyChanged(nameof(Locations));
+        }
+    }
+    #endregion
+
+    #region Location
+    private Location _location;
+    public Location Location
+    {
+        get => _location;
+        set
+        {
+            Location = value;
+            OnPropertyChanged(nameof(Location));
+        }
+    }
+    #endregion
 }
 
--- Compass/Compass/Views/Pages/MapPage.xaml.cs
+++ Compass/Compass/Views/Pages/MapPage.xaml.cs
@@ -1,14 +1,30 @@
-﻿namespace Compass.Views.Pages;
+﻿using Compass.ViewModels;
+
+namespace Compass.Views.Pages;
 
 public partial class MapPage : ContentPage
 {
-	public MapPage()
+    private readonly MapViewModel _viewmodel;
+
+	public MapPage(MapViewModel mapViewModel)
 	{
 		InitializeComponent();
-	}
 
-    protected override void OnAppearing()
+        _viewmodel = mapViewModel;
+        BindingContext = _viewmodel;
+    }
+
+    protected async override void OnAppearing()
     {
         base.OnAppearing();
+        await _viewmodel.OnNavigatedTo();
+    }
+
+    async void Pin_MarkerClicked(System.Object sender, Microsoft.Maui.Controls.Maps.PinClickedEventArgs e)
+    {
+        if(sender is Microsoft.Maui.Controls.Maps.Pin pin) {
+            await _viewmodel.OnPinClickedAsync(long.Parse(pin.Address));
+
+        }
     }
 }
