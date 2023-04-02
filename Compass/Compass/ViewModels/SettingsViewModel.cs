2557fff761c5846eca8d2362fa96412ac0038ac9
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-04-02 19:07:44 +0200

    add switch theme setting

--- Compass/Compass/ViewModels/SettingsViewModel.cs
+++ Compass/Compass/ViewModels/SettingsViewModel.cs
@@ -8,6 +8,8 @@ public class SettingsViewModel : BaseViewModel
     public SettingsViewModel(INavigationService navigationService) : base(navigationService)
     {
         GoSettingsCommand = new Command(OnGoSettingsCommand);
+
+        IsDarkMode = Application.Current.UserAppTheme == AppTheme.Dark;
     }
 
 
@@ -17,6 +19,35 @@ public class SettingsViewModel : BaseViewModel
     }
 
 
-    public string Version => AppInfo.Current.VersionString;
+    private void ThemeChanged(bool isLight) => Application.Current.UserAppTheme = isLight ? AppTheme.Dark : AppTheme.Light;
+
+
+    public string Version { get; } = AppInfo.Current.VersionString;
+
+    #region IsSystemeTheme
+    private bool _isSystemTheme;
+    public bool IsSystemeTheme
+    {
+        get => _isSystemTheme;
+        set {
+            _isSystemTheme = value;
+            OnPropertyChanged(nameof(IsSystemeTheme));
+        }
+    }
+    #endregion
+
+    #region IsDarkMode
+    private bool _isDarkMode;
+    public bool IsDarkMode
+    {
+        get => _isDarkMode;
+        set
+        {
+            _isDarkMode = value;
+            OnPropertyChanged(nameof(IsDarkMode));
+            ThemeChanged(value);
+        }
+    }
+    #endregion
 }
 
