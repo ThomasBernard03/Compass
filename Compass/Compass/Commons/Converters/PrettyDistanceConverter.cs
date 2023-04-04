43b2f6f2cb6a162c4021ded5c065ec6f49bbd902
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-04-04 20:36:59 +0200

    display pretty distances

---
+++ Compass/Compass/Commons/Converters/PrettyDistanceConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Compass.Commons.Converters;
+
+public class PrettyDistanceConverter : IValueConverter
+{
+	public PrettyDistanceConverter()
+	{
+	}
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null || !(value is int distance))
+            return string.Empty;
+
+        if (distance < 1000)
+        {
+            return $"{distance}m";
+        }
+        else
+        {
+            double distanceInKm = distance / 1000.0;
+            return $"{distanceInKm:0.0}km";
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
